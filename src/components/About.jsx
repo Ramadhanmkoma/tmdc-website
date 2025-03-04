94b819b8a6a23e4ae9b1747da27f908396150986
Ramadhanmkoma <ramadhanmkoma9@gmail.com>
2025-03-04 12:25:13 +0300

    About us id added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,7 @@ const features = [
 const About = () => {
     return (
         <>
-            <div className="relative isolate overflow-hidden bg-transparent px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+            <div id='about' className="relative isolate overflow-hidden bg-transparent px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
                 <div className="absolute inset-0 -z-10 overflow-hidden">
                     <svg
                         aria-hidden="true"
