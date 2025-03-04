d6bc951784b761746707ef30889da4b963d26bfb
Ramadhanmkoma <ramadhanmkoma9@gmail.com>
2025-03-04 11:50:53 +0300

    Pwa Feature updated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,54 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "autoUpdate",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Tanzania Muslims Developer Community",
+    short_name: "TMDC",
+    description:
+      "Connecting Muslim developers to share knowledge and build together.",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+    ],
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    scope: "/",
+    orientation: "portrait",
+  },
+  devOptions: {
+    enabled: true,
+  },
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-	react(),
-	tailwindcss(),
-],
-})
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugin)],
+});
